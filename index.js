a58bdf4d4504b6e00f878eb74f960d28e6cde46e
Dima Snisarenko <snisarenkodima@gmail.com>
2017-04-08 12:12:51 +0200

    Add posts

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const componentLibrary = require('component-library');
 
 componentLibrary(
-    'styleguide',
+    'styleguide/templates',
     'dist',
     {
+        layout: 'styleguide/layout.html',
         baseUrl: '/habrahabr/dist/',
         css: [
-            'https://habracdn.net/habr/styles/_build/global_main.css'
+            'https://habracdn.net/habr/styles/_build/global_main.css',
+            'https://habracdn.net/habr/styles/_parts/posts.css'
         ]
 
     }
